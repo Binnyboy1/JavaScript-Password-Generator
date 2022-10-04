715f3f34ad8296a2ecda96df460e537d75a5062d
Binny <ibinstock@outlook.com>
2022-10-03 16:39:47 -0800

    added working prompts for character type selection in JavaScript

--- script.js
+++ script.js
@@ -7,21 +7,58 @@ var specialList = `\`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~`;
 
 // Variable set up
 var length = 0;     // Must be between 8 and 128
-var lower = false;
 lowerList = lowerList.split('');
-var upper = false;
 upperList = upperList.split('');
-var num = false;
 var numList = "0123456789".split('');
-var special = false;
 specialList = specialList.split('');
+masterList = [];
 
 // Prompt questions
 function generatePassword() {
     length = window.prompt("How long should the password be?\n(must be at least 8 characters and no more than 128 characters)");
 
+    // break case #1
     if (length < 8 || length > 128) {
         window.alert("invalid character length");
+        return;
+    }
+
+    window.alert("Please select which character types you want to include.\n(must select at least 1 type)");
+    var lowerPrompt = window.confirm("Click OK to confirm including lowercase letters.");
+    if (lowerPrompt) {
+        window.alert("lowercase letters will be included");
+        masterList = masterList.concat(lowerList);
+    } else {
+        window.alert("lowercase letters will NOT be included");
+    }
+
+    var upperPrompt = window.confirm("Click OK to confirm including uppercase letters.");
+    if (upperPrompt) {
+        window.alert("uppercase letters will be included");
+        masterList = masterList.concat(upperList);
+    } else {
+        window.alert("uppercase letters will NOT be included");
+    }
+
+    var numPrompt = window.confirm("Click OK to confirm including numbers.");
+    if (numPrompt) {
+        window.alert("numbers will be included");
+        masterList = masterList.concat(numList);
+    } else {
+        window.alert("numbers will NOT be included");
+    }
+
+    var specialPrompt = window.confirm("Click OK to confirm including special characters.");
+    if (specialPrompt) {
+        window.alert("special characters will be included");
+        masterList = masterList.concat(specialList);
+    } else {
+        window.alert("special characters will NOT be included");
+    }
+
+    // break case #2
+    if (!masterList.length) {
+        window.alert("must select at least one type");
         return;
     }
 
